31b17575f189a07abf335ea37394b62cd6ee4d90
yemrealtanay <y.emrealtanay@gmail.com>
2021-08-05 18:20:33 +0300

    ön yüz tasarımları eklendi modeller ve Controller'lar hazır

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,57 @@
+<footer class="w-full border-t bg-white pb-12">
+    <div
+        class="relative w-full flex items-center invisible md:visible md:pb-12"
+        x-data="getCarouselData()"
+    >
+        <button
+            class="absolute bg-blue-800 hover:bg-blue-700 text-white text-2xl font-bold hover:shadow rounded-full w-16 h-16 ml-12"
+            x-on:click="decrement()">
+            &#8592;
+        </button>
+        <template x-for="image in images.slice(currentIndex, currentIndex + 6)" :key="images.indexOf(image)">
+            <img class="w-1/6 hover:opacity-75" :src="image">
+        </template>
+        <button
+            class="absolute right-0 bg-blue-800 hover:bg-blue-700 text-white text-2xl font-bold hover:shadow rounded-full w-16 h-16 mr-12"
+            x-on:click="increment()">
+            &#8594;
+        </button>
+    </div>
+    <div class="w-full container mx-auto flex flex-col items-center">
+        <div class="flex flex-col md:flex-row text-center md:text-left md:justify-between py-6">
+            <a href="#" class="uppercase px-3">About Us</a>
+            <a href="#" class="uppercase px-3">Privacy Policy</a>
+            <a href="#" class="uppercase px-3">Terms & Conditions</a>
+            <a href="#" class="uppercase px-3">Contact Us</a>
+        </div>
+        <div class="uppercase pb-6">&copy; posts.com</div>
+    </div>
+</footer>
+
+<script>
+    function getCarouselData() {
+        return {
+            currentIndex: 0,
+            images: [
+                'https://source.unsplash.com/collection/1346951/800x800?sig=1',
+                'https://source.unsplash.com/collection/1346951/800x800?sig=2',
+                'https://source.unsplash.com/collection/1346951/800x800?sig=3',
+                'https://source.unsplash.com/collection/1346951/800x800?sig=4',
+                'https://source.unsplash.com/collection/1346951/800x800?sig=5',
+                'https://source.unsplash.com/collection/1346951/800x800?sig=6',
+                'https://source.unsplash.com/collection/1346951/800x800?sig=7',
+                'https://source.unsplash.com/collection/1346951/800x800?sig=8',
+                'https://source.unsplash.com/collection/1346951/800x800?sig=9',
+            ],
+            increment() {
+                this.currentIndex = this.currentIndex === this.images.length - 6 ? 0 : this.currentIndex + 1;
+            },
+            decrement() {
+                this.currentIndex = this.currentIndex === this.images.length - 6 ? 0 : this.currentIndex - 1;
+            },
+        }
+    }
+</script>
+
+</body>
+</html>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Posts - Aklında Ne Varsa Post'la</title>
+    <meta name="author" content="David Grzyb">
+    <meta name="description" content="">
+
+    <!-- Tailwind -->
+    <link href="https://unpkg.com/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+    <style>
+        @import url('https://fonts.googleapis.com/css?family=Karla:400,700&display=swap');
+
+        .font-family-karla {
+            font-family: karla;
+        }
+    </style>
+
+    <!-- AlpineJS -->
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+    <!-- Font Awesome -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
+</head>
+<body class="bg-white font-family-karla">
+
+    <!-- Top Bar Nav -->
+    <nav class="w-full py-4 bg-blue-800 shadow">
+        <div class="w-full container mx-auto flex flex-wrap items-center justify-between">
+            
+            <nav>
+                @guest
+                <ul class="flex items-center justify-between font-bold text-sm text-white uppercase no-underline">
+                    <li><a class="hover:text-gray-200 hover:underline px-4" href="{{ route('login') }}">Giriş Yap</a></li>
+                    <li><a class="hover:text-gray-200 hover:underline px-4" href="{{ route('register') }}">Üye Ol</a></li>
+                </ul>
+                @endguest
+                @auth
+
+                <div class="hidden sm:flex sm:items-center sm:ml-6">
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button class="flex items-center text-sm font-medium text-white hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                                <div>{{ Auth::user()->name }}</div>
+    
+                                <div class="ml-1">
+                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                            </button>
+                        </x-slot>
+    
+                        <x-slot name="content">
+                            <!-- Authentication -->
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+    
+                                <x-dropdown-link :href="route('logout')"
+                                        onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                                    {{ __('Log Out') }}
+                                </x-dropdown-link>
+                            </form>
+                        </x-slot>
+                    </x-dropdown>
+                </div>
+
+                @endauth
+            </nav>
+
+            <div class="flex items-center text-lg no-underline text-white pr-6">
+                <a class="" href="#">
+                    <i class="fab fa-facebook"></i>
+                </a>
+                <a class="pl-6" href="#">
+                    <i class="fab fa-instagram"></i>
+                </a>
+                <a class="pl-6" href="#">
+                    <i class="fab fa-twitter"></i>
+                </a>
+                <a class="pl-6" href="#">
+                    <i class="fab fa-linkedin"></i>
+                </a>
+            </div>
+        </div>
+
+    </nav>
+
+    <!-- Text Header -->
+    <header class="w-full container mx-auto">
+        <div class="flex flex-col items-center py-12">
+            <a class="font-bold text-gray-800 uppercase hover:text-gray-700 text-5xl" href="#">
+                <a href="{{ url('/') }}" ><img src="./img/logo.fw.png" alt="Posts" width="265"></a>
+            </a>
+            <p class="text-lg text-gray-600">
+                Aklında Ne Varsa Post'la Gitsin!
+            </p>
+        </div>
+    </header>
